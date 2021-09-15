b483751b8db8aeb857d848a42d0533f08de34b6e
Derek <singhavi2122@gmail.com>
2021-09-16 00:03:52 +0530

    favicon commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/navbar.js';
+import Textform from './Components/textform.js';
+import About from './Components/about.js';
+import Alert from './Components/alert.js';
+import React, { useState } from 'react'
+
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      typ: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "rgb(56, 56, 56)";
+      document.body.style.color = "azure";
+      showAlert("Successfully Enabled Dark Mode", "success");
+    }
+    else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      showAlert("Successfully Enabled Light Mode", "success");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <Router> */}
+      <Navbar title="Text-Utils" about="About" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert}/>
+      {/* <Switch>
+        <Route path="/about"> */}
+          <About mode={mode}/>
+        {/* </Route>
+        <Route path="/"> */}
+          <Textform mode={mode} showAlert={showAlert}/>
+        {/* </Route>
+      </Switch>
+    </Router> */}
+    </>
   );
 }
 
