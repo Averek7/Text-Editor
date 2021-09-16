42ebf1668355a82aed0539a792a9a616ace66120
Derek <singhavi2122@gmail.com>
2021-09-16 10:19:36 +0530

    js commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import Navbar from './Component/navbar.js';
 import Textform from './Component/textform.js';
-// import About from './Component/about.js';
+import About from './Component/about.js';
 import Alert from './Component/alert.js';
 import React, { useState } from 'react'
 
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
 
 
 function App() {
@@ -44,20 +44,26 @@ function App() {
   }
   return (
     <>
-    {/* <Router> */}
+    <Router>
       <Navbar title="Text-Utils" about="About" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert}/>
-      {/* <Switch>
-        <Route path="/about"> */}
-          {/* <About mode={mode}/> */}
-        {/* </Route>
-        <Route path="/"> */}
+      <Switch>
+        <Route path="/about">
+          <About mode={mode}/>
+        </Route>
+        <Route path="/">
           <Textform mode={mode} showAlert={showAlert}/>
-        {/* </Route>
+        </Route>
       </Switch>
-    </Router> */}
+    </Router>
     </>
   );
 }
 
 export default App;
+
+// GitHub-Deployment 
+// "homepage": "https://Averek7.github.io/Text-Editor",
+
+// "predeploy": "npm run build",
+// "deploy": "gh-pages -d build",
